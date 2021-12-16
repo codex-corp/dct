3e74e5153999b166e87d48b203ab83eea4b4cd24
Hany <hany.alsamman@gmail.com>
2021-12-16 04:16:19 +0400

    Routes: add register, login and questions.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+# API routes for register
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+
+# API routes for login
+/**
+ * {
+    "success": true,
+    "data": {
+        "access_token": "**",
+        "token_type": "Bearer"
+    },
+    "message": "Hi admin, Welcome to DCT support center!"
+    }
+ */
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('questions', [\App\Http\Controllers\QuestionsController::class, 'createQuestion']);
+
+    Route::get('questions', [\App\Http\Controllers\QuestionsController::class, 'listQuestions']);
+
+    Route::get('questions/{id}', [\App\Http\Controllers\QuestionsController::class, 'getQuestion']);
+
+    Route::put('questions/{id}', [\App\Http\Controllers\QuestionsController::class, 'updateQuestion']);
+
+    # API routes for logout
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 });
+
+//Route::apiResource('questions', questions::class);
+
+//Route::get('/questions', function () {
+//
+//})->middleware(['auth:sanctum', 'ability:check-questions,create-questions']);
