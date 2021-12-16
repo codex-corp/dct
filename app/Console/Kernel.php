40f17cb58637fbe6207ed02635cde8030e526d9c
Hany <hany.alsamman@gmail.com>
2021-12-16 15:24:39 +0400

    Change the question status to "answered" if not replied for 24h

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Models\Question;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -10,12 +12,26 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            $questions = Question::withCount(['answers' => function ($query) {
+                $query->where('created_at', '>=', Carbon::now()->subHours(24));
+            }])->get();
+
+            if (!$questions->isEmpty()) {
+                foreach ($questions as $question) {
+                    if ($question->answers_count > 0) {
+                        //update status
+                    }
+                }
+            }
+        })->hourly();
     }
 
     /**
