9a780e4dd06dca228ff621be6d1baa7054a81233
Hany <hany.alsamman@gmail.com>
2021-12-16 15:26:03 +0400

    set the relation between answers to questions

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -12,4 +12,14 @@ class Answer extends Model
     protected $fillable = [
         'user_id', 'question_id', 'comment'
     ];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
